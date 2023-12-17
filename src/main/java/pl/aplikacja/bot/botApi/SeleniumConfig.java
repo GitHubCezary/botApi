12a8ef6aa5706da9116d5fb37ca238f9c62e65eb
GitHubCezary <cezary.konowrocki@wp.pl>
2023-12-17 01:33:10 +0100

    find by element selenium changes

--- src/main/java/pl/aplikacja/bot/botApi/SeleniumConfig.java
+++ src/main/java/pl/aplikacja/bot/botApi/SeleniumConfig.java
@@ -5,37 +5,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Sleeper;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
 
 @Service
 public class SeleniumConfig {
 
     private String url = "https://www.twitch.tv/tazasi";
 
-
+    @Scheduled(fixedRate = 5000)
     public void printLinks() throws InterruptedException {
-
         ChromeOptions chromeOptions = new ChromeOptions();
 //        chromeOptions.addArguments("--headless");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Cezary\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(url);
 
-//        WebElement sf = driver.findElement(By.id("L2AGLb"));
-//        sf.click();
-//        WebElement pusty = driver.findElement(By.cssSelector("textarea.gLFyf"));
-//        pusty.sendKeys("Smieszne koty");
-//        Sleeper.SYSTEM_SLEEPER.sleep(Duration.ofSeconds(3));
-//        pusty.submit();
-//        Thread.sleep(3000);
-//        WebElement firstLink = driver.findElement(By.cssSelector("div#search a"));
-//
-//        String firstLinkUrl = firstLink.getAttribute("href");
-//        System.out.println("Pierwszy link w wynikach wyszukiwania: " + firstLinkUrl);
-//        driver.quit();
+        WebElement twitchElement = driver.findElement(By.xpath("//div[contains(@aria-label,'')]"));
+        String channelStatusText = twitchElement.getText();
+        if (twitchElement != null) {
+            System.out.println("znaleziono : " + channelStatusText);
+        } else
+            System.out.println("nie znalezione");
     }
 }
